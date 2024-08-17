f8cd2412e61c37455b1b82e8aef26c7a301e0782
Navaneetha Krishnan P <157093273+Navaneetha-Krishnan-P@users.noreply.github.com>
2024-08-17 11:32:28 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,7 +10,7 @@ const ProductDetails = require("./models/productsSchema");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:["https://market-for-rental-equipments-e-commerce-website-mern-v5yl.vercel.app"]}));
 app.use(parser.json());
 
 
